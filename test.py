6faaef41e01e98636402f97ba87577e05a0c0c34
Thomas Ballinger <thomasballinger@gmail.com>
2016-09-02 17:18:30 -0400

    py3 compat

--- test.py
+++ test.py
@@ -3,6 +3,10 @@ import os
 import sys
 
 
+if sys.version_info[0] == 2:
+    input = raw_input
+
+
 class ForkUndoConsole(code.InteractiveConsole):
     def __init__(self):
         code.InteractiveConsole.__init__(self)
@@ -20,11 +24,11 @@ class ForkUndoConsole(code.InteractiveConsole):
     def raw_input(self, prompt=""):
         while True:  # each time through this loop is another
             try:
-                s = raw_input(prompt)
+                s = input(prompt)
             except EOFError:
-                self.die_and_tell_parent('exit')
+                self.die_and_tell_parent(b'exit')
             if s == 'undo':
-                self.die_and_tell_parent('done')
+                self.die_and_tell_parent(b'done')
             read_fd, write_fd = os.pipe()
             pid = os.fork()
             is_child = pid == 0
@@ -42,9 +46,11 @@ class ForkUndoConsole(code.InteractiveConsole):
                 # e is the first letter of 'exit'
                 if from_child == 'e':
                     # propogate that message up
-                    self.die_and_tell_parent('exit')
+                    self.die_and_tell_parent(b'exit')
+
 
 if __name__ == '__main__':
+    print('parent pid:', os.getpid())
     console = ForkUndoConsole()
     # console.interact() does something like
     # while True:
