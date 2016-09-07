a172083143b161eb57e64c05bb13d471d330c0d0
Thomas Ballinger <thomasballinger@gmail.com>
2016-09-07 13:08:05 -0400

    forget pipes, there's no information to send back

--- test.py
+++ test.py
@@ -1,22 +1,15 @@
 import code, os, sys
 
-write_to_parent_fd = None
-
 def read_line(prompt=""):
-    global write_to_parent_fd
     while True:
         s = input(prompt)
         if s == 'undo':
-            os.write(write_to_parent_fd, b'\n')
             sys.exit()  # die to resume parent
-        read_from_child_fd, write_fd = os.pipe()
-        if os.fork():  # if this is the parent
-            # blocking read to wait for child
-            os.read(read_from_child_fd, 1)
-        else:  # this is the child
-            # store how to talk to parent
-            write_to_parent_fd = write_fd
+        pid = os.fork()
+        if pid == 0:  # if this is the child
             return s
+        else:  # if this is the parent
+            os.wait()
 
 console = code.InteractiveConsole()
 while True:
