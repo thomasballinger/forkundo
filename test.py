9733c418da5424cfa8238da4201de7940b8bcf1f
Thomas Ballinger <thomasballinger@gmail.com>
2016-09-07 12:39:54 -0400

    manual loop

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ class ForkUndoConsole(code.InteractiveConsole):
         self.write_to_parent_fd = None
         self.read_from_child_fd = None
 
-    def raw_input(self, prompt=""):
+    def read_line(self, prompt=""):
         while True:  # each time through this loop is another
             s = input(prompt)
             if s == 'undo':
@@ -17,22 +17,17 @@ class ForkUndoConsole(code.InteractiveConsole):
                 sys.exit()
             read_fd, write_fd = os.pipe()
 
-            if os.fork():
+            if os.fork():  # this is the parent
                 self.read_from_child_fd = read_fd
-
                 # blocking read to wait for child to die
                 os.read(self.read_from_child_fd, 1)
-            else:
+            else:  # this is the parent
                 self.write_to_parent_fd = write_fd
                 return s
 
 
 if __name__ == '__main__':
-    print('parent pid:', os.getpid())
     console = ForkUndoConsole()
-    # console.interact() does something like
-    # while True:
-    #     source = self.raw_input('>>> ')
-    #     self.run_source(source)
-    # but works with multiline input
-    console.interact()
+    while True:
+        src = console.read_line('>>> ')
+        console.runsource(src)
