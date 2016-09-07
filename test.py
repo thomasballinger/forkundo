1da54d198404fb06bbd8286799508b35c7363089
Thomas Ballinger <thomasballinger@gmail.com>
2016-09-07 12:45:03 -0400

    no class

--- test.py
+++ test.py
@@ -2,32 +2,28 @@ import code
 import os
 import sys
 
+write_to_parent_fd = None
+read_from_child_fd = None
 
-class ForkUndoConsole(code.InteractiveConsole):
-    def __init__(self):
-        code.InteractiveConsole.__init__(self)
-        self.write_to_parent_fd = None
-        self.read_from_child_fd = None
-
-    def read_line(self, prompt=""):
-        while True:  # each time through this loop is another
-            s = input(prompt)
-            if s == 'undo':
-                os.write(self.write_to_parent_fd, b'done\n')
-                sys.exit()
-            read_fd, write_fd = os.pipe()
-
-            if os.fork():  # this is the parent
-                self.read_from_child_fd = read_fd
-                # blocking read to wait for child to die
-                os.read(self.read_from_child_fd, 1)
-            else:  # this is the parent
-                self.write_to_parent_fd = write_fd
-                return s
+def read_line(prompt=""):
+    global write_to_parent_fd
+    global read_from_child_fd
+    while True:  # each time through this loop is another
+        s = input(prompt)
+        if s == 'undo':
+            os.write(write_to_parent_fd, b'done\n')
+            sys.exit()
+        read_fd, write_fd = os.pipe()
+        if os.fork():  # this is the parent
+            read_from_child_fd = read_fd
+            os.read(read_from_child_fd, 1)
+        else:  # this is the parent
+            write_to_parent_fd = write_fd
+            return s
 
 
 if __name__ == '__main__':
-    console = ForkUndoConsole()
+    console = code.InteractiveConsole()
     while True:
-        src = console.read_line('>>> ')
+        src = read_line('>>> ')
         console.runsource(src)
