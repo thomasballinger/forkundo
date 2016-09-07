b16fdef7619a5c8bf2eb74ac052d22891a5c3f6f
Thomas Ballinger <thomasballinger@gmail.com>
2016-09-07 12:52:33 -0400

    add comments

--- test.py
+++ test.py
@@ -3,21 +3,20 @@ import os
 import sys
 
 write_to_parent_fd = None
-read_from_child_fd = None
 
 def read_line(prompt=""):
     global write_to_parent_fd
-    global read_from_child_fd
-    while True:  # each time through this loop is another
+    while True:
         s = input(prompt)
         if s == 'undo':
-            os.write(write_to_parent_fd, b'done\n')
-            sys.exit()
-        read_fd, write_fd = os.pipe()
-        if os.fork():  # this is the parent
-            read_from_child_fd = read_fd
+            os.write(write_to_parent_fd, b'\n')
+            sys.exit()  # die to resume parent
+        read_from_child_fd, write_fd = os.pipe()
+        if os.fork():  # if this is the parent
+            # blocking read to wait for child
             os.read(read_from_child_fd, 1)
-        else:  # this is the parent
+        else:  # this is the child
+            # store how to talk to parent
             write_to_parent_fd = write_fd
             return s
 
